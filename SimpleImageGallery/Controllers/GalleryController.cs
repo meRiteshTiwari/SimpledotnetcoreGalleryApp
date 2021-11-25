1f2b52b30d9a580a1b2ea965c7c211b22f405fc2
RI20092307 <ritesh.tiwari2@wipro.com>
2021-11-25 16:43:42 +0530

    Added hardcoded Image list and created simple image view

--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data.Models;
 using SimpleImageGallery.Models;
 using System;
 using System.Collections.Generic;
@@ -9,12 +10,63 @@ namespace SimpleImageGallery.Controllers
 {
     public class GalleryController : Controller
     {
+        
         public IActionResult Index()
         {
-            var model = new GalleryIndexModel(){
+            var hikingImageTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
 
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
             };
-            return View();
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Urban",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "New York",
+                Id = 2
+            };
+
+            hikingImageTags.Add(tag1);
+            cityImageTags.AddRange(new List<ImageTag>{ tag2,tag3});
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Hiking Trip",
+                    Url= "https://images.pexels.com/photos/10325555/pexels-photo-10325555.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "On The Trail",
+                    Url= "https://images.pexels.com/photos/10298360/pexels-photo-10298360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+                new GalleryImage()
+                {
+                    Title = "Downtown",
+                    Url= "https://images.pexels.com/photos/9089028/pexels-photo-9089028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+                }
+            };
+            var model = new GalleryIndexModel()
+            {
+                Images = imageList,
+                SearchQuery = ""
+            };
+            return View(model);
         }
     }
 }
