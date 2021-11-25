0622187575faa424587d6064bc9f97f9347c0126
RI20092307 <ritesh.tiwari2@wipro.com>
2021-11-25 13:50:24 +0530

    Created Data project. GalleryController, added connection to sql. ran initial migration.

---
+++ SimpleImageGallery.Data/SimpleImageGalleryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleImageGallery.Data.Models;
+
+namespace SimpleImageGallery.Data
+{
+    public class SimpleImageGalleryDbContext : DbContext
+    {
+        public SimpleImageGalleryDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<ImageTag> ImageTags { get; set; }
+    }
+}
---
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GalleryIndexModel(){
+
+            };
+            return View();
+        }
+    }
+}
