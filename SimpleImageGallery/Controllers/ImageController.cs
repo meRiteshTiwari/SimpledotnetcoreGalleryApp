6d982eb42e35ab4b9469018cd8bbdf0b58fb4bba
RI20092307 <ritesh.tiwari2@wipro.com>
2021-11-26 13:02:44 +0530

    created basic upload page and controller and model to upload image

---
+++ SimpleImageGallery/Controllers/ImageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleImageGallery.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
